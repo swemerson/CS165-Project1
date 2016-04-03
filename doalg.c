e8294dc5a7944a8379829b50c6eca1da56b6e85c
Sean <swemerson@gmail.com>
2016-04-03 04:22:29 -0700

    Rework of doalg, passes both rounds
    n=  100,  k=10:  maximum= 325,  avg=  242.29
    n=10000,  k=40:  maximum= 16054,  avg=14900.08

--- doalg.c
+++ doalg.c
@@ -1,51 +1,44 @@
 #include <limits.h>
-#include "COMPARE.c"
 
-void sort_array(int *best, int size)
+int doalg(int n, int k, int *Best)
 {
-	int i;
-	for(i = size-2; i >= 0; i--)
-	{
-		int x = number[best[i]];
-		int j = i + 1;
-		while(j <= size && number[best[j]] > x)
-		{
-			number[best[j-1]] = number[best[j]];
-			j++;
-		}
-		number[best[j+1]] = x;
-	}
-}
+    if (n < 10 || n > 10000 || k < 1 || k > 100)
+		return 0;
 
-int *doalg(int n, int k, int *best)
-{
-	int i, j;
-	int low = 0;
-	int high = k-1;
-	for(i = 0; i < k; i++)
-	{
-		best[i] = i;
-	}
-	sort_array(best, k);
-	for(i = k; i < n; i++)
-	{
-		if(number[i] > number[best[k-1]])
-		{
-			while(low <= high)
-			{
-				int mid = low + (high-low)/2;
-				if(number[best[mid]] > number[i] && number[i] > number[best[mid+1]])
-				{
-					int counter = mid+1;
-					while(counter < k-1)
-					{
-						best[counter+1] = best[counter];
-						counter++;
-					}
-					best[mid+1] = i;
-				}
-			}
-		}
-	}
-	return best;
+    int i, j, l, val;
+    
+    // Insert first k element indices into Best in decreasing order
+    Best[0] = 1;
+
+    // i is the index of the private array (1-based)
+    for (i = 2; i <= k; ++i)
+    {
+        // Start at end of existing elements in Best and find where to insert next element
+        for (j = i - 1; j > 0; --j)
+        {
+            if ((val = COMPARE(i, Best[j - 1])) == -1) return 0;
+            if (val == 2) break;			
+        }
+
+		// Shift elements over to insert next element
+        for (l = i - 1; l > j; --l)
+            Best[l] = Best[l - 1];
+        Best[j] = i;
+    }
+
+    // Check indices after k, insert in descending order, same logic 
+    for (i = k + 1; i <= n; ++i)
+    {
+        for (j = k; j > 0; --j)
+        {
+            if ((val = COMPARE(i, Best[j - 1])) == -1) return 0;
+            if (val == 2) break;			
+        }
+
+        for (l = k; l > j; --l)
+            Best[l] = Best[l - 1];
+        Best[j] = i;
+    }  
+    
+    return 1;
 }
