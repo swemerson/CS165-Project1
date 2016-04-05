c860143f6d3a5bac978ec0263e70acab43f74c5d
Sean <swemerson@gmail.com>
2016-04-05 03:25:24 -0700

    Now uses binary search
    n=  100,  k=10:  maximum= 230,  avg=  189.85
    n=10000,  k=40:  maximum= 11550,  avg=11311.13

--- doalg.c
+++ doalg.c
@@ -1,43 +1,125 @@
-#include <limits.h>
-
 int doalg(int n, int k, int *Best)
 {
+	// Out of bounds argument check
     if (n < 10 || n > 10000 || k < 1 || k > 100)
 		return 0;
 
-    int i, j, l, val;
+    int i, j, l, lo, hi, val;
     
     // Insert first k element indices into Best in decreasing order
-    Best[0] = 1;
+    
+	// Insert 1st
+	Best[0] = 1;	
+	
+	// Insert 2nd
+	if ((val = COMPARE(2, Best[0])) == -1) 
+		return 0;
+	if (val == 2)
+		Best[1] = 2;
+	else
+	{
+		Best[1] = 1;
+		Best[0] = 2;
+	}
+
+	// Insert 3rd
+	if ((val = COMPARE(3, Best[0])) == -1) 
+		return 0;
+	if (val == 1)
+	{
+		Best[2] = Best[1];
+		Best[1] = Best[0];
+		Best[0] = 3;
+	}
+	else
+	{
+		if ((val = COMPARE(3, Best[1])) == -1)
+			return 0;
+		if (val == 1)
+		{
+			Best[2] = Best[1];
+			Best[1] = 3;
+		}
+		else
+			Best[2] = 3;
+	}
 
     // i is the index of the private array (1-based)
-    for (i = 2; i <= k; ++i)
+    for (i = 4; i <= k; ++i)
     {
-        // Start at end of existing elements in Best and find where to insert next element
-        for (j = i - 1; j > 0; --j)
-        {
-            if ((val = COMPARE(i, Best[j - 1])) == -1) return 0;
-            if (val == 2) break;			
-        }
+		// Using binary search to find where to insert next element (in descending order)
+		lo = 0;
+		hi = i - 2;
+		while (lo < hi - 1)
+		{
+			int mid = lo + (hi - lo) / 2;
+            if ((val = COMPARE(i, Best[mid])) == -1) 
+				return 0;
+            if (val == 2)
+				lo = mid;
+			else
+				hi = mid;
+		}
+
+		// Case where element is largest
+		if (lo == 0)
+		{
+            if ((val = COMPARE(i, Best[lo])) == -1) 
+				return 0;
+            if (val == 1)
+				hi = 0;
+		}
+
+		// Case where element is smallest 
+		else if (hi == i - 2)
+		{
+            if ((val = COMPARE(i, Best[hi])) == -1) 
+				return 0;
+            if (val == 2)
+				++hi;
+		}
 
 		// Shift elements over to insert next element
-        for (l = i - 1; l > j; --l)
-            Best[l] = Best[l - 1];
-        Best[j] = i;
+		for (l = i - 1; l > hi; --l)
+			Best[l] = Best[l - 1];
+		Best[hi] = i;
     }
 
     // Check indices after k, insert in descending order, same logic 
     for (i = k + 1; i <= n; ++i)
     {
-        for (j = k; j > 0; --j)
-        {
-            if ((val = COMPARE(i, Best[j - 1])) == -1) return 0;
-            if (val == 2) break;			
-        }
+        // Check if element too small for Best
+		if ((val = COMPARE(i, Best[k - 1])) == -1) 
+			return 0;
+		if (val == 2)
+			continue;
+	
+		lo = 0;
+		hi = k - 1;
+		while (lo < hi - 1)
+		{
+			int mid = lo + (hi - lo) / 2;
+            if ((val = COMPARE(i, Best[mid])) == -1) 
+				return 0;
+            if (val == 2)
+				lo = mid;
+			else
+				hi = mid;
+		}
 
-        for (l = k; l > j; --l)
-            Best[l] = Best[l - 1];
-        Best[j] = i;
+		// Case where element is largest
+		if (lo == 0)
+		{
+            if ((val = COMPARE(i, Best[lo])) == -1) 
+				return 0;
+            if (val == 1)
+				hi = 0;
+		}
+
+		// Shift elements over to insert next element
+		for (l = k; l > hi; --l)
+			Best[l] = Best[l - 1];
+		Best[hi] = i;
     }  
     
     return 1;
