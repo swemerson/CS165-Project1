f316fef541a5d994392198d67898d12c83ea4c42
kanansasaki <krsasaki@uci.edu>
2016-04-11 19:03:37 -0700

    Binary Search with some compartments

--- doalg.c
+++ doalg.c
@@ -1,103 +1,57 @@
-int doalg(int n, int k, int *Best)
+#include <stdlib.h>
+
+void insert_first_three(int n, int k, int *Best)
 {
-	// Out of bounds argument check
-    if (n < 10 || n > 10000 || k < 1 || k > 100)
-		return 0;
-    int i, j, l, lo, hi, val;
-	int temp[10000];
-	int pos;
-	
-	if (n / 2 >= k)
-	{
-		for (i = 1; i < n; i += 2)
-		{
-			pos = 0;
-			if (n % 2 != 0)
-				temp[pos++] = i++;									
-			if ((val = COMPARE(i, i + 1)) == -1)
-				return 0;
-			if (val == 1)
-				temp[pos++] = i;
-			else
-				temp[pos++] = i + 1;
-		}
+    int val;
+    // Insert 1st
+	Best[0] = 1;
 
-		if (n % 2 != 0) 
-			n = n / 2 + 1;
-		else
-			n /= 2;
-	}
-
-	while (n / 2 >= k)
-	{
-		for (i = 0; i < n - 1; i += 2)
-		{
-			pos = 0;
-			if (n % 2 != 0)
-				temp[pos++] = temp[i++];									
-			if ((val = COMPARE(temp[i], temp[i + 1])) == -1)
-				return 0;
-			if (val == 1)
-				temp[pos++] = temp[i];
-			else
-				temp[pos++] = temp[i + 1];
-		}
-
-		if (n % 2 != 0) 
-			n = n / 2 + 1;
-		else
-			n /= 2;
-	}
-
-
-    // Insert first k element indices into Best in decreasing order
-    
-	// Insert 1st
-	Best[0] = temp[0];	
-	
 	// Insert 2nd
-	if ((val = COMPARE(temp[1], Best[0])) == -1) 
+	if ((val = COMPARE(2, Best[0])) == -1)
 		return 0;
 	if (val == 2)
-		Best[1] = temp[1];
+		Best[1] = 2;
 	else
 	{
-		Best[1] = temp[0];
-		Best[0] = temp[1];
+		Best[1] = 1;
+		Best[0] = 2;
 	}
 
 	// Insert 3rd
-	if ((val = COMPARE(temp[2], Best[0])) == -1) 
+	if ((val = COMPARE(3, Best[0])) == -1)
 		return 0;
 	if (val == 1)
 	{
 		Best[2] = Best[1];
 		Best[1] = Best[0];
-		Best[0] = temp[2];
+		Best[0] = 3;
 	}
 	else
 	{
-		if ((val = COMPARE(temp[2], Best[1])) == -1)
+		if ((val = COMPARE(3, Best[1])) == -1)
 			return 0;
 		if (val == 1)
 		{
 			Best[2] = Best[1];
-			Best[1] = temp[2];
+			Best[1] = 3;
 		}
 		else
-			Best[2] = temp[2];
+			Best[2] = 3;
 	}
+}
 
-    // i is the index of the private array (1-based)
-    for (i = 3; i < k; ++i)
+void insert_until_k(int n, int k, int *Best)
+{
+    int i, l, lo, hi, val;
+    for (i = 4; i <= k; ++i)
     {
 		// Using binary search to find where to insert next element (in descending order)
 		lo = 0;
-		hi = temp[i] - 2;
+		hi = i - 2;
 		while (lo < hi - 1)
 		{
 			int mid = lo + (hi - lo) / 2;
-            if ((val = COMPARE(temp[i], Best[mid])) == -1) 
+            if ((val = COMPARE(i, Best[mid])) == -1)
 				return 0;
             if (val == 2)
 				lo = mid;
@@ -108,42 +62,45 @@ int doalg(int n, int k, int *Best)
 		// Case where element is largest
 		if (lo == 0)
 		{
-            if ((val = COMPARE(temp[i], Best[lo])) == -1) 
+            if ((val = COMPARE(i, Best[lo])) == -1)
 				return 0;
             if (val == 1)
 				hi = 0;
 		}
 
-		// Case where element is smallest 
-		else if (hi == temp[i] - 2)
+		// Case where element is smallest
+		else if (hi == i - 2)
 		{
-            if ((val = COMPARE(temp[i], Best[hi])) == -1) 
+            if ((val = COMPARE(i, Best[hi])) == -1)
 				return 0;
             if (val == 2)
 				++hi;
 		}
 
 		// Shift elements over to insert next element
-		for (l = temp[i] - 1; l > hi; --l)
+		for (l = i - 1; l > hi; --l)
 			Best[l] = Best[l - 1];
-		Best[hi] = temp[i];
+		Best[hi] = i;
     }
+}
 
-    // Check indices after k, insert in descending order, same logic 
-    for (i = k; i < n; ++i)
+void insert_rest(int n, int k, int *Best)
+{
+    int i, l, lo, hi, val;
+    for (i = k + 1; i <= n; ++i)
     {
         // Check if element too small for Best
-		if ((val = COMPARE(temp[i], Best[k - 1])) == -1) 
+		if ((val = COMPARE(i, Best[k - 1])) == -1)
 			return 0;
 		if (val == 2)
 			continue;
-	
+
 		lo = 0;
 		hi = k - 1;
 		while (lo < hi - 1)
 		{
 			int mid = lo + (hi - lo) / 2;
-            if ((val = COMPARE(temp[i], Best[mid])) == -1) 
+            if ((val = COMPARE(i, Best[mid])) == -1)
 				return 0;
             if (val == 2)
 				lo = mid;
@@ -154,7 +111,7 @@ int doalg(int n, int k, int *Best)
 		// Case where element is largest
 		if (lo == 0)
 		{
-            if ((val = COMPARE(temp[i], Best[lo])) == -1) 
+            if ((val = COMPARE(i, Best[lo])) == -1)
 				return 0;
             if (val == 1)
 				hi = 0;
@@ -163,8 +120,27 @@ int doalg(int n, int k, int *Best)
 		// Shift elements over to insert next element
 		for (l = k; l > hi; --l)
 			Best[l] = Best[l - 1];
-		Best[hi] = temp[i];
-    }  
-    
+		Best[hi] = i;
+    }
+}
+
+int doalg(int n, int k, int *Best)
+{
+	// Out of bounds argument check
+    if (n < 10 || n > 10000 || k < 1 || k > 100)
+		return 0;
+
+    // Insert first k element indices into Best in decreasing order
+
+	insert_first_three(n, k, Best);
+
+    // i is the index of the private array (1-based)
+
+    insert_until_k(n, k, Best);
+
+    // Check indices after k, insert in descending order, same logic
+
+    insert_rest(n, k, Best);
+
     return 1;
 }
